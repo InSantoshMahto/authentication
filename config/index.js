9bc48226f82330f733cc0659db94f704a863e709
santosh mahto <mahtos480@gmail.com>
2019-10-20 16:15:56 +0530

    config file converted into jspon

--- config/index.js
+++ config/index.js
@@ -4,15 +4,27 @@ const path = require('path');
 if (process.env.NODE_ENV === 'production') {
   // production config
   console.info('INFO: Running on PRODUCTION Environments');
-  module.exports = require('./key_prod.config');
+  module.exports = JSON.parse(
+    fs.readFileSync(path.join(__dirname, 'key_prod.config.json'), {
+      encoding: 'utf-8',
+    })
+  );
 } else if (process.env.NODE_ENV === 'development') {
   // development config
   console.info('INFO: Running on DEVELOPMENT Environments');
-  module.exports = require('./key_dev.config');
+  module.exports = JSON.parse(
+    fs.readFileSync(path.join(__dirname, 'key_dev.config.json'), {
+      encoding: 'utf-8',
+    })
+  );
 } else {
   // local config
   console.info('INFO: Running on LOCAL Environments');
-  module.exports = require('./key_local.config');
+  module.exports = JSON.parse(
+    fs.readFileSync(path.join(__dirname, 'key_local.config.json'), {
+      encoding: 'utf-8',
+    })
+  );
 }
 
 // export constants.
